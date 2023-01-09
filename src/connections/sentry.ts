f74cbeb0663b1b5d42cd56ef18f3217882d56bd2
Shindou Mihou <git@mihou.pw>
2023-01-10 05:02:14 +0800

    fix: sentry error

--- src/connections/sentry.ts
+++ src/connections/sentry.ts
@@ -1,19 +1,13 @@
 import {Module} from "./types/module.js";
 import {needs} from "../utils/env.js";
 import * as Sentry from '@sentry/node';
-import {getInfo} from "discord-hybrid-sharding";
 
 export const SentryModule: Module = {
     name: 'sentry',
     init: async () => {
         const { SENTRY_DSN } = needs('SENTRY_DSN') as { SENTRY_DSN: string }
         Sentry.init({
-            dsn: SENTRY_DSN,
-            beforeSend: (event, _) => {
-                // @ts-ignore
-                event.extra.cluster = getInfo().CLUSTER
-                return event
-            }
+            dsn: SENTRY_DSN
         })
     }
 }
